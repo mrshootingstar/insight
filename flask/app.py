fd90a1d1f1a0213937174a620d3e74ced845ac45
moDell9560 <m.mirghorbani@gmail.com>
2018-09-20 19:19:09 -0700

    First Commit

---
+++ flask/app.py
@@ -0,0 +1,119 @@
+import os
+import cv2
+import time
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['mov', 'mp4'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        success, image = self.video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+def detect_cars(filename):
+    print("Detecting cars on ".format(filename))
+    face_cascade = cv2.CascadeClassifier('./haar-cascades/cars.xml')
+    video_path = os.path.join("./uploads", filename)
+    print("Video Path: ".format(video_path))
+    target_video_path = os.path.join("./static", filename)
+    print("target vp: " , target_video_path)
+    time.sleep(30)
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
+    out = cv2.VideoWriter('output.mp4',fourcc, 15.0, (1280,360))
+    vc = cv2.VideoCapture(video_path)
+    print("Bismillah")
+    if vc.isOpened():
+        print("Bismillah1")
+        rval , frame = vc.read()
+    else:
+        print("Bismillah2")
+        rval = False
+    
+    frameIndex = 0
+    while True:
+        frameIndex += 1
+        rval, frame = vc.read()
+        if not rval: break
+        
+        frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5) 
+        # car detection.
+        cars = face_cascade.detectMultiScale(frame, 1.1, 2)
+    
+        ncars = 0
+        for (x,y,w,h) in cars:
+            print("Bismillah {} --> ".format(frameIndex, len(cars)))
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+            ncars = ncars + 1
+ 
+        # cv2.imshow('image',frame)
+
+        out.write(frame)
+    
+    print("Finished")
+    vc.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('watch_file',
+                                    filename=filename))
+    return render_template('index.html')
+
+                    
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename, as_attachment=True)
+
+
+@app.route('/watch/<filename>')
+def watch_file(filename):
+    video_path = url_for('static', filename=filename)
+    print(video_path)
+    return render_template('watch.html', video_path=video_path)
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
